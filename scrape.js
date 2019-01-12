3392ac4e3bd261166fe8ad009f6fcd586969bd54
datluongquang <35949060+datluongquang@users.noreply.github.com>
2019-01-12 03:16:31 -0500

    new mongodb database

--- scrape.js
+++ scrape.js
@@ -1,9 +1,11 @@
 let request = require('request');
 let cheerio= require('cheerio');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/YourDB";
 let CSList=[];
 let CSLink=[];
 var CSRequirement=new Map();
-var a=0;
+var a={a:'b'};
 request("http://bulletin.miamioh.edu/engineering-computing/computer-science-bs/", function (error, response, html) {
     if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
@@ -13,9 +15,29 @@ request("http://bulletin.miamioh.edu/engineering-computing/computer-science-bs/"
             CSLink.push(link);
             CSList.push(a);
         });
-        console.log(CSList);
+        MongoClient.connect(url,{ useNewUrlParser: true }, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("mydb");
+            dbo.collection("customers").removeMany({});
+            for(var i=0;i<CSList.length;i++){
+                a={Name:CSList[i],Link:CSLink[i]};
+                dbo.collection("customers").insertOne(a);
+            }
+            db.close();
+        });
     }
 });
+
+
+// MongoClient.connect(url,{ useNewUrlParser: true }, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mydb");
+//     dbo.collection("customers").find({}).toArray(function(err, result) {
+//         if (err) throw err;
+//         console.log(result);
+//         db.close();
+//     });
+// });
 function forward(url) {
     var pre=[];
     request2(url, function (error, response, html) {
